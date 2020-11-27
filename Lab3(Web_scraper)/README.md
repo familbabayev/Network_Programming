0bdfe86b6b0e5f5b4a24b5e7e90e759e99b6ea96
familbabayev <familbabayev@users.noreply.github.com>
2020-11-27 19:44:12 +0400

    first commit

---
+++ Lab3(Web_scraper)/README.md
@@ -0,0 +1,41 @@
+# Client-Server based TCP web scraper console application
+
+## Scenario
+You as a software developer must create the client-server-based console app “web_scraper”.
+With the next abilities:
+* Get statistical data: Calculate the number of pictures in the webpage
+* Calculate the number of the leaf paragraphs in the webpage
+Task:
+Create console-based app with two roles server and the client. The server must be started and wait the
+request from the client. Server must produce the web scraping of the webpage to get two parameters:
+the number of pictures and the number of the leaf paragraphs. The leaf paragraphs in HTML document
+represents only the last paragraphs in the nested paragraph structures (picture below marks with blue
+the paragraphs).
+<p>
+/ \
+<p> <p>
+/ \
+<p> <p>
+The client must send the request to the server to get the proper answer. The client has options page (-
+p) to get the statistical data. All the cacluation must be done on the server side
+
+## Installation
+```
+git clone https://github.com/familbabayev/Network_Programming/tree/master/Lab3(Web_scraper)
+```
+
+Install the packages in the requirements.txt
+
+## Usage
+The default value(1060) for port number is assigned by the program, so if you want you can omit it.
+
+For server:
+```
+python3 web_scraper.py server "" [-PORT]
+```
+Here "" represent 0.0.0.0 and means server can accept connections from all interfaces.
+
+For client:
+```
+python3 web_scraper.py client [HOSTNAME] [-PORT] -p [Webpage URL]
+```
---
+++ Lab3(Web_scraper)/web_scraper.py
@@ -0,0 +1,100 @@
+import argparse, socket
+import sys, requests
+from bs4 import BeautifulSoup
+
+
+class Server:
+    MAX_BYTES = 65535
+
+    def __init__(self, interface, port):
+        self.interface = interface
+        self.port = port
+
+    def get_image_count(self, link_html):
+        return len(link_html.find_all('img'))
+
+    def get_leaf_p_count(self, link_html):
+        n = 0
+        for p in link_html.find_all('p'):
+            if not p.find_all('p'):
+                n += 1
+        return n
+
+    def scrape_link(self, url):
+        webpage = requests.get(url)
+        soup = BeautifulSoup(webpage.text, "html.parser")
+        img_count = self.get_image_count(soup)
+        p_count = self.get_leaf_p_count(soup)
+        return (img_count, p_count)
+
+    def run(self):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        sock.bind((self.interface, self.port))
+        sock.listen(1)
+        print("Listening at", sock.getsockname())
+        while True:
+            sc, sockname = sock.accept()
+            print("We have accepted a connection from", sockname)
+            print(" Socket name", sc.getsockname())
+            print(" Socket peer", sc.getpeername())
+
+            data = sc.recv(self.MAX_BYTES)
+            url = data.decode("utf-8")
+
+            scraped_link = self.scrape_link(url)
+            img_count, p_count = scraped_link[0], scraped_link[1]
+
+            response = f"{img_count} {p_count}"
+            sc.sendall(response.encode("utf-8"))
+
+            sc.close()
+            print(" Reply sent, socket closed")
+
+
+
+class Client:
+    MAX_BYTES = 65535
+
+    def __init__(self, hostname, port):
+        self.hostname = hostname
+        self.port = port
+
+    def run(self, url):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((self.hostname, self.port))
+
+        sock.sendall(url.encode("utf-8"))
+
+        server_reply = (sock.recv(self.MAX_BYTES)).decode("utf-8")
+        img_count, p_count = server_reply.split()
+
+        print("Reply from Server:")
+        print("Number of images : ", img_count)
+        print("Number of leaf paragraphs : ", p_count)
+
+
+def main():
+    choices = {'client':Client, 'server':Server}
+    parser = argparse.ArgumentParser('Scrape web pages')
+    parser.add_argument('role', choices=choices, help='which role to play')
+    parser.add_argument('host', help='interface the server listens at;'
+                       ' host the client sends to')
+    parser.add_argument('-port', metavar='PORT', type=int, default=1060,
+                       help='TCP port (default 1060)')
+
+    if sys.argv[1] == "client":
+        parser.add_argument('-p', metavar='PAGE', type = str, help = "link to the webpage")
+
+    args = parser.parse_args()
+    clss = choices[args.role]
+    
+    if args.role == "client":
+        clss(args.host, args.port).run(args.p)
+    else:
+        clss(args.host, args.port).run()
+
+
+if __name__ == "__main__":
+    main()
+
